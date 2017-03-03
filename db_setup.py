c7faf6b29d82c8ce0a1f1d93a77bc560cff0f9d6
Ashwani Kumar <ashwanikumar31165@gmail.com>
2017-03-03 10:05:30 +0530

    Added DB Objects

--- db_setup.py
+++ db_setup.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.types import Date 
+import datetime
 
 Base = declarative_base()
 
@@ -18,7 +20,31 @@ class Admin(Base):
     password = Column(String(250), nullable = False)
 
 
+class Ballot(Base):
+    __tablename__ = 'ballot'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable = False)
+    date = Column(Date, nullable = False)
+    uid = Column(Integer, ForeignKey('admin.id'))
+    admin = relationship(Admin)
 
+    
+class Options(Base):
+    __tablename__ = 'option'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable = False)
+    bid = Column(Integer, ForeignKey('ballot.id'))
+    ballot = relationship(Ballot)
 
+class Voters(Base):
+    __tablename__ = 'voter'
+    
+    id = Column(Integer, primary_key=True)
+    uname = Column(String(250), nullable = False)
+    mobile = Column(Integer, nullable = False)
+    password = Column(String(250), nullable = False)
+    
 engine = create_engine('sqlite:///OBallot.db')
 Base.metadata.create_all(engine)
